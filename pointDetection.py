827c5f82d9b3dc2a5c9cbe7dc71ada32e6d7e3d8
Colin Willoughby <colinw@savix.net>
2015-02-04 20:18:03 -0500

    Detecting X and Y distances

--- pointDetection.py
+++ pointDetection.py
@@ -2,6 +2,8 @@ import rr
 import math
 from collections import namedtuple
 
+#Axis m1013 field of view 62.38 degrees in the x direction
+
 c = rr.GetArrayVariable("MEQ_COORDINATES")
 
 #Define named tuples for better self-documentation of our point array
@@ -23,7 +25,7 @@ try:
     topDistance = distance(points.topRight, points.topLeft)
     bottomDistance = distance(points.bottomRight, points.bottomLeft)
     widthAve = average([topDistance, bottomDistance])
-    
+
     leftDistance = distance(points.topLeft, points.bottomLeft)
     rightDistance = distance(points.topRight, points.bottomRight)
     heightAve = average([leftDistance, rightDistance])
@@ -43,16 +45,21 @@ try:
     centerPoint = Point(centerX, centerY)
     angle = math.atan2(centerPoint.y - bottomCenter.y, centerPoint.x - bottomCenter.x)
     angle -= math.pi/2
+
     if widthAve > heightAve:
         if angle < 0:
             angle = math.pi/2 + angle
+            bottomCenter = Point(average([points.topRight.x, points.bottomRight.x]), average([points.topRight.y, points.bottomRight.y]))
         else:
             angle = -math.pi/2 + angle
-    
-    rr.SetVariable("Tote Angle", round(angle * 180 / math.pi, 0))
-    rr.SetArrayVariable("Bottom Center", list(bottomCenter) + list(centerPoint))
-    
-    
+            bottomCenter = Point(average([points.bottomLeft.x, points.topLeft.x]), average([points.bottomLeft.y, points.topLeft.y]))
+
+    rr.SetVariable("Tote Angle", angle)
+    rr.SetArrayVariable("Bottom Center Line", list(bottomCenter) + list(centerPoint))
+
+    rr.SetVariable("ToteX", (5.42/math.cos((math.pi/180)*(11+0.078*bottomCenter.y)))*math.tan((math.pi/180)*0.078*(bottomCenter.x-400)))
+    rr.SetVariable("ToteY", 5.42*math.tan((math.pi/180)*(11+(0.078*bottomCenter.y))))
+
     #VERY IMPORTANT: must use list() around the points, or RoboRealms will crash!
     #Probably caused by it getting upset over the tuple being a namedtuple
     rr.SetArrayVariable("Top Right", list(points.topRight))
@@ -60,6 +67,6 @@ try:
     rr.SetArrayVariable("Bottom Left", list(points.bottomLeft))
     rr.SetArrayVariable("Bottom Right", list(points.bottomRight))
     rr.SetVariable("Visible", True)
-except (TypeError, IndexError, ZeroDivisionError):
+except (TypeError, IndexError, ZeroDivisionError) as e:
     #This will run when the tote is not visible to the camera
     rr.SetVariable("Visible", False)
